8893c35165fddc1d731b81bb56fb40ad30436d9e
amarsree <amarsree007@gmail.com>
2023-07-02 14:58:39 +0530

    registration [wip]

--- app/Http/Controllers/API/RegisterController.php
+++ app/Http/Controllers/API/RegisterController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers\API;
 
-use Illuminate\Http\Request;
-use App\Http\Controllers\API\BaseController as BaseController;
-use App\Models\User;
-use Illuminate\Support\Facades\Auth;
 use Validator;
+use App\Models\User;
+use App\Models\Profile;
+use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Auth;
 use App\Http\Requests\RegisterRequest;
+use App\Http\Controllers\API\BaseController as BaseController;
 
 class RegisterController extends BaseController
 {
@@ -19,23 +22,36 @@ class RegisterController extends BaseController
      */
     public function register(RegisterRequest $request): JsonResponse
     {
+        try {
+            $validated_input = $request->validated();
+        
+            // save data user table
+            DB::beginTransaction();
+            $validated_input['password'] = bcrypt($validated_input['password']);
+            $validated_input['name']=$validated_input['first_name']." " .$validated_input['last_name'];
+            $user = User::create($validated_input);
+            // update the role to user
+            $user->assignRole('user');
+            
+            $validated_input['user_id'] = $user->id;
+            $validated_input['status'] = 'pending';
 
-        $validated = $request->validated();
+            // save data in profiles table
+            $profile = Profile::create($validated_input);
+            
+            DB::commit();
+            // save data in payments if on paypal or cards else send email to user for zelle payment
+    
+           $data['user'] = $user;
+           $data['profile'] = $profile;
+    
+            return $this->sendResponse($data, 'User register successfully.');
 
-        dd($validated);
-
-        // save data user table
-        // save data in profiles table
-        // update the role to user
-        // save data in payments if on paypal or cards else send email to user for zelle payment
-
-        $input = $request->all();
-        $input['password'] = bcrypt($input['password']);
-        $user = User::create($input);
-        $success['token'] =  $user->createToken('atmiya')->plainTextToken;
-        $success['name'] =  $user->name;
-
-        return $this->sendResponse($success, 'User register successfully.');
+        } catch(\Exception $e) {
+            DB::rollBack();
+            return $this->sendError($e, ["internal server error"], 500);
+        }
+      
     }
 
     /**
@@ -50,11 +66,35 @@ class RegisterController extends BaseController
             $user = Auth::user();
             $success['token'] =  $user->createToken('atmiya')->plainTextToken;
             $success['name'] =  $user->name;
+            $success['profile'] =  $user->profile;
 
             return $this->sendResponse($success, 'User login successfully.');
         }
         else{
             return $this->sendError('Unauthorised.', ['error'=>'Unauthorised']);
+        }
+    }
+
+     /**
+     * get api
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function get(Request $request): JsonResponse
+    {
+        $id = (int) $request->input('id', '');
+        Log::debug($id);
+        $user = User::find($id);
+
+        if(!empty($user)) {
+            $data['user'] = $user;
+            $data['profile'] = $user->profile;
+
+
+            return $this->sendResponse($data, '');
+
+        }else{
+            return $this->sendError('No user found.', ['error'=>'No user found']);
         }
     }
 }
--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Requests;
 
-use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Support\Facades\Log;
 
+use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Exceptions\HttpResponseException;
 
@@ -26,11 +27,41 @@ class RegisterRequest extends FormRequest
      */
     public function rules(): array
     {
+        Log::info('New registration request: '.json_encode($this->all()));
         return [
-            'name' => 'bail|required',
-            'email' => 'required|email',
-            'password' => 'required',
-            'confirm_password' => 'required|same:password',
+             //Personal Details
+             'reference_by' => 'bail|required|string|max:50',
+             'reference_phone' => 'bail|required|regex:/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/|string',
+             'first_name' => 'bail|required|string|max:50',
+             'last_name' => 'bail|required|string|max:50',
+             'email' => 'bail|email|max:100|unique:users,email',
+             'phone' => 'bail|nullable|regex:/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/|unique:profiles,phone',
+             'marital_status' => 'bail|required|in:single,married',
+             'gender' => 'bail|required|in:male,female',
+
+             'password' => 'required|string|min:6|max:50',
+             'confirm_password' => 'required|same:password',
+             
+             //spouse details
+             'spouse_first_name' => 'bail|required|string|max:50',
+             'spouse_last_name' => 'bail|required|string|max:50',
+             'spouse_email' => 'bail|required|email|string|max:50',
+             'spouse_phone' => 'bail|required|string|nullable|regex:/^\(?(\d{3})\)?[- ]?(\d{3})[- ]?(\d{4})$/|max:50',
+             'family_members' => 'bail|array|nullable',
+
+             //Address
+             'address_line_1' => 'bail|required|string|max:200',
+             'address_line_2' => 'bail|sometimes|nullable|string|max:200',
+             'city' => 'bail|required|string|max:100',
+             'state' => 'bail|required|integer|min:1|exists:chapter_states,id',
+             'metro_area' => 'bail|sometimes|nullable|integer|min:1|exists:metro_areas,id',
+             'zip_code' => 'bail|required|string|max:25',
+             'country' => 'bail|required|string|max:25',
+             
+             //Membership Category
+             'membership_category' => 'bail|required|integer|min:1|exists:membership_categories,id',
+             
+             'payment_mode' => 'bail|required|string|max:25',
         ];
     }
 
